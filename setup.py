5b81f7ddafe740d3e1071b0a1bbb2e837af67b3c
Keith Bussell <kbussell@gmail.com>
2017-01-08 19:23:46 -0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@ from setuptools import setup
 
 setup(
     name="django-jsonfield-compat",
-    version='0.1.0',
+    version='0.2.0',
     description="Compatability layer between django-jsonfield and Django's native JSONField",
     long_description=open("README.md").read(),
     url="http://github.com/kbussell/django-jsonfield-compat/",
     author="Keith Bussell",
     author_email="kbussell@gmail.com",
-    packages = [
+    packages=[
         "jsonfield_compat",
     ],
-    classifiers = [
+    classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Framework :: Django',
     ],
-    test_suite='tests.main',
-    include_package_data=True,
+    zip_safe=False,
 )
